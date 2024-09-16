3afc83e15d2426364574930015928ecb733a1854
faris808 <fariszama2019@gmail.com>
2024-09-16 20:25:31 +0530

    Fixed backend code deployment issue

--- src/socket.js
+++ src/socket.js
@@ -6,5 +6,5 @@ export const initSocket=async()=>{  //ye function basically client ka ek instanc
         timeout:10000,
         transports:['websocket'],
     };
-    return io(process.env.REACT_APP_BACKEND_URL,options);   //It will return the instance of socket client
+    return io("https://codingbuddy-web-application.onrender.com",options);   //It will return the instance of socket client
 };
