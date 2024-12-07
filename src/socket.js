ddd4db848cfcfd57340fe7da2d6c0250193b2b5d
faris808 <fariszama2019@gmail.com>
2024-12-07 21:24:53 +0530

    changed backend url

--- src/socket.js
+++ src/socket.js
@@ -6,5 +6,5 @@ export const initSocket=async()=>{  //ye function basically client ka ek instanc
         timeout:10000,
         transports:['websocket'],
     };
-    return io("https://codingbuddy-web-application.onrender.com",options);   //It will return the instance of socket client
+    return io("https://codingbuddyapp.up.railway.app/",options);   //It will return the instance of socket client
 };
